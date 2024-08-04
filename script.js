99292ae46f8a42536d0fe5e86bb793ace98400d1
Schnitzel541 <j.budding122@gmail.com>
2024-08-04 21:02:03 +0200

    Add functionality to play the game over 5 rounds, while keeping the score and declaring a winner at the end

--- script.js
+++ script.js
@@ -1,57 +1,85 @@
-let winningMessage = "You Win!"
-let losingMessage = "You lose!"
-
-let rpsValues = {
-    0: "Rock",
-    1: "Paper",
-    2: "Scissors"
-}
-
-
-const getComputerChoice = () => {
-    let max = 3;
-    let computerRandomChoice = Math.floor(Math.random() * max)
-    return rpsValues[computerRandomChoice];
-}
-
-
-const getHumanChoice = () => {
-    let computerChoice = getComputerChoice()
-    let humanChoice = prompt("Please enter a valid choice (Rock, Paper or Scissors)")
-
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!")
+// Getting choices from either contestants
+function getComputerChoice() {
+    let choices = {
+        0: "rock",
+        1: "paper",
+        2: "scissors"
     }
-    else {
-    switch(humanChoice) {
-        
-        case("Rock" || "rock"):
-        if (computerChoice === "Paper") {
-            console.log(losingMessage) }
-        if (computerChoice === "Scissors") {
-            console.log(winningMessage) }
-            break
 
-        case("Paper" || "paper"):
-        if (computerChoice === "Scissors") {
-            console.log(losingMessage) }
-        if (computerChoice == "Rock") {
-            console.log(winningMessage) }
-            break
+    let max = 3;
+    let randomNumber = Math.floor(Math.random() * max)
 
-        case("Scissors" || "scissors"):
-        if (computerChoice === "Rock") {
-            console.log(losingMessage) }
-        if (computerChoice === "Paper") {
-            console.log(winningMessage) }
-            break
-        default:
-            alert("Please pick a valid option")
-            computerChoice = "-"
-            getHumanChoice();
+    let randomChoice = choices[randomNumber];
+    return(randomChoice);
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Please enter a valid choice (Rock, Paper or Scissors)").toLowerCase();
+    return(humanChoice);
+}
+
+function playGame() {
+    // Score keeping
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+
+        if (humanChoice == computerChoice) {
+            console.log("It's a tie!")
+        }
+        else {
+            switch(humanChoice) {
+                case ("rock"):
+                    if (computerChoice === "paper") {
+                        console.log("You lose!");
+                        computerScore++;
+                    }
+                    else if (computerChoice === "scissors") {
+                        console.log("You win!");
+                        humanScore++;
+                    }
+                    break;
+                
+                case ("paper"):
+                    if (computerChoice === "scissors") {
+                        console.log("You lose!");
+                        computerScore++;
+                    }
+                    else if (computerChoice === "rock") {
+                        console.log("You win!");
+                        humanScore++;
+                    }
+                    break;
+                
+                case ("scissors"):
+                    if (computerChoice === "rock") {
+                        console.log("You lose!")
+                        computerScore++;
+                    }
+                    else if (computerChoice === "paper") {
+                        console.log("You win!")
+                        humanScore++;
+                    }
+                    break;
+            }
         }
     }
-    console.log(`The computer picked: ${computerChoice}`)
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(humanSelection, computerSelection))
+        console.log(`Human score: ${humanScore}`)
+        console.log(`Computer score: ${computerScore}`)
+    }
+    if (humanScore > computerScore) {
+        console.log("Human wins!")
+    }
+    else if (humanScore < computerScore) {
+        console.log("Computer wins!")
+    }
+    else {
+        console.log("It's a tie")
+    }
 }
-
-getHumanChoice();
+playGame()
